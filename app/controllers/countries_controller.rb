918ab8b28f86d783044a2d596cfb841543477904
AustynT <Austyntravis9@gmail.com>
2018-02-26 10:01:42 -0700

    added index,create,show methods for country

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,20 @@
 class CountriesController < ApplicationController
+
+  def index
+    @countries = Country.includes(:states).all
+  end
+
+  def create
+    @country = Country.new(country_params)
+  end
+
+  def show
+    @country = Country.find_by_name(params[:name])
+  end
+
+  private
+
+  def country_params
+    params.require(:country).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'countries/:name', to: 'countries#show'
   resources :states
-  resources :countries
+  resources :countries, only: [:show, :index]
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
