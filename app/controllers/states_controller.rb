aca14d01f4b9553002b1bc438144cf0570fadc33
AustynT <Austyntravis9@gmail.com>
2018-02-26 11:36:28 -0700

    built out the states controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,2 +1,32 @@
 class StatesController < ApplicationController
+  def index
+    @states = State.all
+  end
+  def new
+    @states = States.new
+  end
+
+  def create
+    @states = States.new(states_params)
+  end
+
+  def show
+    @state = State.find(params[:id])
+  end
+
+  def edit
+    @state = State.find(params[:id])
+  end
+
+  def destroy
+    @state.destroy
+    redirect_to states_path
+  end
+
+  private
+
+  def state_params
+    params.require(:state).permit(:name)
+  end
+
 end
