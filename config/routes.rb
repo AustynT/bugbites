bd95f0d24e3af6d03b0c4c4bca22a0525c5c0515
AustynT <Austyntravis9@gmail.com>
2018-02-27 11:38:15 -0700

    did the devise install

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :states
   resources :countries, except: [:show]
   resources :blogs
+
+
+  root to: "countries#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
