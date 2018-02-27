f31c413a62076925ad72cbdbcb18441193aa2117
AustynT <Austyntravis9@gmail.com>
2018-02-27 12:06:27 -0700

    made static controller for pages, set home to root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/about_me'
+
   devise_for :users
   resources :states
   resources :countries, except: [:show]
   resources :blogs
 
 
-  root to: "countries#index"
+  root to: "static_pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
