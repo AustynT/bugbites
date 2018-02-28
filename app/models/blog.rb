fe57fb660a28d0972c92fd264ab4fd9fa75a70cd
AustynT <Austyntravis9@gmail.com>
2018-02-28 10:45:52 -0700

    completed admin page, and started blog index

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,10 @@ class Blog < ApplicationRecord
   belongs_to :state
   has_one :country, through: :state
 
+
+  after_initialize :set_defaults
+  
+  def set_defaults
+    self.main_photo = "http://placehold.it/300x250"
+  end
 end
